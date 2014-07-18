39f8a0bef98be4594b947dd7237a3b64dc8c8028
moozzyk <pawelka@microsoft.com>
2014-07-18 16:21:51 -0700

    AntiFlakinessLeague - adding additional logging to nail down flaky tests

--- tests/Microsoft.AspNet.SignalR.Client.Store.Tests/FunctionalTests/EndToEndTests.cs
+++ tests/Microsoft.AspNet.SignalR.Client.Store.Tests/FunctionalTests/EndToEndTests.cs
@@ -1,10 +1,13 @@
 ﻿
+using System.IO;
+using System.Text;
 using Microsoft.AspNet.SignalR.Client.Transports;
 using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
+
 
 namespace Microsoft.AspNet.SignalR.Client.Store.Tests
 {
@@ -18,91 +21,121 @@ namespace Microsoft.AspNet.SignalR.Client.Store.Tests
             const int MessageCount = 3;
             var sentMessages = new List<string>();
             var receivedMessages = new List<string>();
-
-            using (var hubConnection = new HubConnection(HubUrl))
+            var traces = new StringBuilder();
+            try
             {
-                var wh = new ManualResetEventSlim();
-
-                var proxy = hubConnection.CreateHubProxy("StoreWebSocketTestHub");
-                proxy.On<string>("echo", m =>
+                using (var hubConnection = new HubConnection(HubUrl))
                 {
-                    receivedMessages.Add(m);
-                    if (receivedMessages.Count == MessageCount)
+         
+                    hubConnection.TraceLevel = TraceLevels.All;
+                    hubConnection.TraceWriter = new StringWriter(traces);
+
+                    hubConnection.Error += e =>
                     {
-                        wh.Set();
+                        throw new InvalidOperationException("Traces: " + traces.ToString(), e);
+                    };
+
+                    var wh = new ManualResetEventSlim();
+
+                    var proxy = hubConnection.CreateHubProxy("StoreWebSocketTestHub");
+                    proxy.On<string>("echo", m =>
+                    {
+                        receivedMessages.Add(m);
+                        if (receivedMessages.Count == MessageCount)
+                        {
+                            wh.Set();
+                        }
+                    });
+
+                    await hubConnection.Start(new WebSocketTransport());
+
+                    for (var i = 0; i < MessageCount; i++)
+                    {
+                        var message = "MyMessage" + i;
+                        await proxy.Invoke("Echo", message);
+                        sentMessages.Add(message);
                     }
-                });
 
-                await hubConnection.Start(new WebSocketTransport());
-
-                for (var i = 0; i < MessageCount; i++)
-                {
-                    var message = "MyMessage" + i;
-                    await proxy.Invoke("Echo", message);
-                    sentMessages.Add(message);
+                    await Task.Run(() => wh.Wait(5000));
                 }
 
-                await Task.Run(() => wh.Wait(5000));
+                Assert.Equal(sentMessages, receivedMessages);
             }
-
-            Assert.Equal(sentMessages, receivedMessages);
+            catch (Exception exception)
+            {
+                // very poor man's tracing
+                throw new InvalidOperationException(traces.ToString(), exception);
+            }
         }
 
         [Fact]
         public async Task WebSocketReconnects()
         {
             var stateChanges = new List<KeyValuePair<ConnectionState, ConnectionState>>();
+            var traces = new StringBuilder();
 
-            using (var hubConnection = new HubConnection(HubUrl))
+            try
             {
-                string receivedMessage = null;
-                var messageReceivedWh = new ManualResetEventSlim();
+                using (var hubConnection = new HubConnection(HubUrl))
+                {   
+                    hubConnection.TraceLevel = TraceLevels.All;
+                    hubConnection.TraceWriter = new StringWriter(traces);
 
-                var proxy = hubConnection.CreateHubProxy("StoreWebSocketTestHub");
-                proxy.On<string>("echo", m =>
-                {
-                    receivedMessage = m;
-                    messageReceivedWh.Set();
-                });
+                    string receivedMessage = null;
+                    var messageReceivedWh = new ManualResetEventSlim();
 
-                hubConnection.StateChanged += stateChanged => stateChanges.Add(
-                    new KeyValuePair<ConnectionState, ConnectionState>(stateChanged.OldState, stateChanged.NewState));
+                    var proxy = hubConnection.CreateHubProxy("StoreWebSocketTestHub");
+                    proxy.On<string>("echo", m =>
+                    {
+                        receivedMessage = m;
+                        messageReceivedWh.Set();
+                        throw new Exception("test");
+                    });
 
-                var reconnectingInvoked = false;
-                hubConnection.Reconnecting += () => reconnectingInvoked = true;
+                    hubConnection.StateChanged += stateChanged => stateChanges.Add(
+                        new KeyValuePair<ConnectionState, ConnectionState>(stateChanged.OldState, stateChanged.NewState));
 
-                var reconnectedWh = new ManualResetEventSlim();
-                hubConnection.Reconnected += reconnectedWh.Set;
+                    var reconnectingInvoked = false;
+                    hubConnection.Reconnecting += () => reconnectingInvoked = true;
 
-                await hubConnection.Start(new WebSocketTransport { ReconnectDelay = new TimeSpan(0, 0, 0, 500)});
+                    var reconnectedWh = new ManualResetEventSlim();
+                    hubConnection.Reconnected += reconnectedWh.Set;
 
-                try
-                {
-                    await proxy.Invoke("ForceReconnect");
+                    await hubConnection.Start(new WebSocketTransport { ReconnectDelay = new TimeSpan(0, 0, 0, 500)});
+
+                    try
+                    {
+                        await proxy.Invoke("ForceReconnect");
+                    }
+                    catch (InvalidOperationException)
+                    {
+                    }
+
+                    Assert.True(await Task.Run(() => reconnectedWh.Wait(5000)));
+                    Assert.True(reconnectingInvoked);
+                    Assert.Equal(ConnectionState.Connected, hubConnection.State);
+
+                    await proxy.Invoke("Echo", "MyMessage");
+                    await Task.Run(() => messageReceivedWh.Wait(5000));
+                    Assert.Equal("MyMessage", receivedMessage);
                 }
-                catch (InvalidOperationException)
-                {
-                }
 
-                Assert.True(await Task.Run(() => reconnectedWh.Wait(5000)));
-                Assert.True(reconnectingInvoked);
-                Assert.Equal(ConnectionState.Connected, hubConnection.State);
-
-                await proxy.Invoke("Echo", "MyMessage");
-                await Task.Run(() => messageReceivedWh.Wait(5000));
-                Assert.Equal("MyMessage", receivedMessage);
+                Assert.Equal(
+                    new[]
+                    {
+                        new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Disconnected, ConnectionState.Connecting),
+                        new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Connecting, ConnectionState.Connected),
+                        new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Connected, ConnectionState.Reconnecting),
+                        new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Reconnecting, ConnectionState.Connected),
+                        new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Connected, ConnectionState.Disconnected),
+                    },
+                    stateChanges);
             }
-
-            Assert.Equal(
-                new[]
-                {
-                    new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Disconnected, ConnectionState.Connecting),
-                    new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Connecting, ConnectionState.Connected),
-                    new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Connected, ConnectionState.Reconnecting),
-                    new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Reconnecting, ConnectionState.Connected),
-                    new KeyValuePair<ConnectionState, ConnectionState>(ConnectionState.Connected, ConnectionState.Disconnected),
-                },
-                stateChanges);
+            catch (Exception exception)
+            {
+                // very poor man's tracing
+                throw new InvalidOperationException(traces.ToString(), exception);
+            }
         }
 
         [Fact]
@@ -110,46 +143,63 @@ namespace Microsoft.AspNet.SignalR.Client.Store.Tests
         {
             var receivedMessage = (string)null;
 
-            using (var hubConnection = new HubConnection(HubUrl))
+            var traces = new StringBuilder();
+            try
             {
-                hubConnection.StateChanged += stateChange =>
+                using (var hubConnection = new HubConnection(HubUrl))
                 {
-                    if (stateChange.OldState == ConnectionState.Connected &&
-                        stateChange.NewState == ConnectionState.Reconnecting)
+                    hubConnection.TraceLevel = TraceLevels.All;
+                    hubConnection.TraceWriter = new StringWriter(traces);
+
+                    hubConnection.Error += e =>
                     {
-                        // Reverting quick timeout 
-                        ((IConnection) hubConnection).KeepAliveData = new KeepAliveData(
-                            timeoutWarning: TimeSpan.FromSeconds(30),
-                            timeout: TimeSpan.FromSeconds(20),
-                            checkInterval: TimeSpan.FromSeconds(2));
-                    }
-                };
+                        throw new InvalidOperationException("Traces: " + traces.ToString(), e);
+                    };
 
-                var reconnectedWh = new ManualResetEventSlim();
-                hubConnection.Reconnected += reconnectedWh.Set;
+                    hubConnection.StateChanged += stateChange =>
+                    {
+                        if (stateChange.OldState == ConnectionState.Connected &&
+                            stateChange.NewState == ConnectionState.Reconnecting)
+                        {
+                            // Reverting quick timeout 
+                            ((IConnection) hubConnection).KeepAliveData = new KeepAliveData(
+                                timeoutWarning: TimeSpan.FromSeconds(30),
+                                timeout: TimeSpan.FromSeconds(20),
+                                checkInterval: TimeSpan.FromSeconds(2));
+                        }
+                    };
 
-                var messageReceivedWh = new ManualResetEventSlim();
-                var proxy = hubConnection.CreateHubProxy("StoreWebSocketTestHub");
-                proxy.On<string>("echo", m =>
-                {
-                    receivedMessage = m;
-                    messageReceivedWh.Set();
-                });
+                    var reconnectedWh = new ManualResetEventSlim();
+                    hubConnection.Reconnected += reconnectedWh.Set;
 
-                await hubConnection.Start(new WebSocketTransport { ReconnectDelay = new TimeSpan(0, 0, 0, 500) });
+                    var messageReceivedWh = new ManualResetEventSlim();
+                    var proxy = hubConnection.CreateHubProxy("StoreWebSocketTestHub");
+                    proxy.On<string>("echo", m =>
+                    {
+                        receivedMessage = m;
+                        messageReceivedWh.Set();
+                    });
 
-                // Setting the values such that a timeout happens almost instantly
-                ((IConnection)hubConnection).KeepAliveData = new KeepAliveData(
-                    timeoutWarning: TimeSpan.FromSeconds(10),
-                    timeout: TimeSpan.FromSeconds(0.5),
-                    checkInterval: TimeSpan.FromSeconds(1)
-                );
+                    await hubConnection.Start(new WebSocketTransport {ReconnectDelay = new TimeSpan(0, 0, 0, 500)});
 
-                Assert.True(reconnectedWh.Wait(5000));
+                    // Setting the values such that a timeout happens almost instantly
+                    ((IConnection) hubConnection).KeepAliveData = new KeepAliveData(
+                        timeoutWarning: TimeSpan.FromSeconds(10),
+                        timeout: TimeSpan.FromSeconds(0.5),
+                        checkInterval: TimeSpan.FromSeconds(1)
+                        );
 
-                await proxy.Invoke("Echo", "MyMessage");
-                Assert.True(messageReceivedWh.Wait(5000));
-                Assert.Equal("MyMessage", receivedMessage);
+                    Assert.True(reconnectedWh.Wait(5000));
+
+                    await proxy.Invoke("Echo", "MyMessage");
+                    Assert.True(messageReceivedWh.Wait(5000));
+                    Assert.Equal("MyMessage", receivedMessage);
+                }
+            }
+            catch (Exception exception)
+            {
+                // very poor man's tracing
+                throw new InvalidOperationException(traces.ToString(), exception);
             }
         }
     }
